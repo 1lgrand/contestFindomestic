5e75abe1b40894f58da84c526d4f0e82353c2e8d
Alessandro De Marzo <demarzoad@libero.it>
2023-03-23 21:26:00 +0100

    Modificato File JSON

--- game/question.js
+++ game/question.js
@@ -2,20 +2,25 @@ var jsonData = [
     {
       "question": "Qual è la capitale di Italia?",
       "answers": [
-        {"answer": "Napoli", "value": 0},
-        {"answer": "Bari", "value": 0},
-        {"answer": "Roma", "value": 1},
-        {"answer": "Lecce", "value": 0}
-      ]
+        {"answer": "Napoli"},
+        {"answer": "Bari"},
+        {"answer": "Roma"},
+        {"answer": "Lecce"}
+      ],
+    
+      "correctAns":"Roma"
+
     },
   
     {
       "question": "Qual è il mio colore preferito? ",
       "answers": [
-        {"answer": "Giallo", "value": 0},
-        {"answer": "Arancione", "value": 1},
-        {"answer": "Viola", "value": 0},
-        {"answer": "Bianco", "value": 0}
-      ]
+        {"answer": "Giallo"},
+        {"answer": "Arancione"},
+        {"answer": "Viola"},
+        {"answer": "Bianco"}
+      ],
+
+      "correctAns":"Arancione"
     }
 ]
