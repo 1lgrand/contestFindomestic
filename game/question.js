10ade5eca8e528a11c6c78e62d4c69a6002128cf
Marco Armenise <armenise.marco@panettipitagora.edu.it>
2023-04-18 22:27:57 +0200

    Aggiunte domande

--- game/question.js
+++ game/question.js
@@ -18,7 +18,7 @@ var jsonData = [
         {"answer": "Le aziende"},
         {"answer": "Lo Stato"},
         {"answer": "Le persone comuni"},
-        {"answer": "La banca"}
+        {"answer": "La Banca"}
       ],
     
       "correctAns":"La Banca"
